33eba69768594a6550f726f4e3de08307a02cec8
liuyunwei <827600210@qq.com>
2017-04-12 16:40:28 +0800

    2.14

--- src/main/java/com/baidu/fis/velocity/tools/JelloTool.java
+++ src/main/java/com/baidu/fis/velocity/tools/JelloTool.java
@@ -1,11 +1,27 @@
 package com.baidu.fis.velocity.tools;
 
+import com.baidu.fis.util.Settings;
+
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializeConfig; 
+import com.alibaba.fastjson.serializer.SimpleDateFormatSerializer;
 import org.apache.velocity.tools.config.DefaultKey;
+import java.util.Date;
 
 @DefaultKey("jello")
 public class JelloTool {
     public static String jsonEncode(Object obj) {
-        return JSON.toJSONString(obj);
+    	String dateFormat = Settings.getString("jsonDateFormat");
+
+    	if(dateFormat == null) {
+    		return JSON.toJSONString(obj);
+    	} else {
+    		return jsonEncode(obj, dateFormat);
+    	}
+    }
+    public static String jsonEncode(Object obj, String dateFormat) {
+    	SerializeConfig mapping = new SerializeConfig();
+    	mapping.put(Date.class, new SimpleDateFormatSerializer(dateFormat)); 
+        return JSON.toJSONString(obj, mapping);
     }
 }
